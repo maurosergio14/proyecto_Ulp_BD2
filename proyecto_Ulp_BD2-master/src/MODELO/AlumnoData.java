4305f76d8bf8a80ce4ecaca887afede3a88b5007
LuisPc-Master <LuisPc-Master@DESKTOP-TOBOU6U>
2020-05-28 18:27:11 -0300

    alumnoData

---
+++ proyecto_Ulp_BD2-master/src/MODELO/AlumnoData.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODELO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Thomas
+ */
+class AlumnoData {
+
+    private Connection conectar = null;
+
+    public AlumnoData(Conexion conexion) throws SQLException {
+        try {
+            conectar = conexion.getConexion();
+        } catch (SQLException ex) {
+            System.out.println("Error al abrir al obtener la conexion");
+        }
+    }
+   
+    
+    
+}
